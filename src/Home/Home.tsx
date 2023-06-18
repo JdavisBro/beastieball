c7ba62131c39115e9656837f7f8e4a5d0e40f579
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2023-06-18 13:53:59 +0100

    Add beastiepedia link to home.

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,7 +1,21 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./Home.module.css";
+
 export default function Home(): React.ReactNode {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Home</h1>
+      <h1
+        className={styles.url}
+        onClick={useCallback(() => {
+          navigate("/beastiepedia/");
+        }, [navigate])}
+      >
+        Beastiepedia
+      </h1>
     </div>
   );
 }

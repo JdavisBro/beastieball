9bfc183f901d13128105cf4a79fe4df761e83e8b
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2023-06-21 01:50:19 +0100

    Update 404

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,21 +1,13 @@
-import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import styles from "./Home.module.css";
+// import styles from "./Home.module.css";
 
 export default function Home(): React.ReactNode {
-  const navigate = useNavigate();
   return (
     <div>
       <h1>Home</h1>
-      <h1
-        className={styles.url}
-        tabIndex={0}
-        onClick={useCallback(() => {
-          navigate("/beastiepedia/");
-        }, [navigate])}
-      >
-        Beastiepedia
+      <h1>
+        <Link to="/beastiepedia/">Beastiepedia</Link>
       </h1>
     </div>
   );
--- src/PageNotFound.tsx
+++ src/PageNotFound.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function PageNotFound(): React.ReactElement {
+  const [col, setCol] = useState(Math.floor(Math.random() * 360));
+  const [spr, setSpr] = useState(Math.floor(Math.random() * 127));
+
+  function handleRandomize() {
+    setCol(Math.floor(Math.random() * 360));
+    setSpr(Math.floor(Math.random() * 127));
+  }
+
+  const style = {
+    "--notfoundface-col": `hsl(${col}, 100%, 40%)`,
+    "--notfoundface-spr": `url(/gameassets/sprEmoji/${spr}.png)`,
+  } as React.CSSProperties;
   return (
-    <div>
-      <h1>404</h1>
+    <div className="notfoundcontainer">
+      <h1>Page Not Found</h1>
+      <div
+        className="notfoundface"
+        tabIndex={0}
+        style={style}
+        onClick={handleRandomize}
+        onKeyDown={(event) => {
+          if (event.key == "Enter") {
+            handleRandomize();
+          }
+        }}
+      ></div>
+      <h1>
+        <Link to="/">Go to Home</Link>
+      </h1>
     </div>
   );
 }

e95101d2f7d50512780f71d9f8389e04529dd06b
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2024-02-05 18:39:17 +0000

    basic beastie preview (only sprecko

---
+++ src/Beastiepedia/Preview/WebGL.ts
@@ -0,0 +1,122 @@
+export default function setupWebGL(canvas: HTMLCanvasElement, vertex: string, fragment: string) {
+    const gl = canvas.getContext("webgl");
+    if (!gl) {
+        throw Error("No WebGL.")
+    }
+    const program = createProgram(gl, vertex, fragment);
+    
+    createTexCoordAttribute(gl, program);
+    createPositionAttribute(gl, program);
+
+    createTexture(gl)
+
+    gl.viewport(-1000, -1000, 2000, 2000);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(program);
+
+    return {gl: gl, program: program};
+}
+
+export function setColorUniforms(gl: WebGLRenderingContext, program: WebGLProgram, colors: number[][]) {
+    for (let i=0; i < (6 < colors.length ? 6 : colors.length); i++) {
+        gl.uniform3fv(gl.getUniformLocation(program, `colorOut[${i}]`), colors[i])
+    }
+    draw(gl);
+} 
+
+function createTexture(gl: WebGLRenderingContext) {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+   
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+}
+
+export function setImage(gl: WebGLRenderingContext, image: HTMLImageElement) {
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    draw(gl);
+}
+
+function draw(gl: WebGLRenderingContext) {
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+}
+
+function createPositionAttribute(gl: WebGLRenderingContext, program: WebGLProgram) {
+    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    const positionBuffer = gl.createBuffer();
+    const positions = [-1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, -1];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+}
+
+function createTexCoordAttribute(gl: WebGLRenderingContext, program: WebGLProgram) {
+    const texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+    var texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        0.0,  0.0,
+        1.0,  0.0,
+        0.0,  1.0,
+        0.0,  1.0,
+        1.0,  0.0,
+        1.0,  1.0,
+    ]), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(texCoordLocation, size, type, normalize, stride, offset);
+  
+}
+
+function createProgram(gl: WebGLRenderingContext, vertex: string, fragment: string) {
+    const vertex_shader = createShader(gl, gl.VERTEX_SHADER, vertex)
+    const fragment_shader = createShader(gl, gl.FRAGMENT_SHADER, fragment)
+    const program = gl.createProgram()
+    if (!program) {
+        throw Error("Couldn't create program.")
+    }
+    gl.attachShader(program, vertex_shader);
+    gl.attachShader(program, fragment_shader);
+    gl.linkProgram(program)
+    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!success) {
+        gl.deleteProgram(program)
+        throw Error(`Program link failed. ${gl.getProgramInfoLog(program)}`)
+    }
+    return program
+}
+
+function createShader(gl: WebGLRenderingContext, type: number, source: string) {
+    const shader = gl.createShader(type)
+    if (!shader) {
+        throw Error("Couldn't create shader.")
+    }
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader);
+    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+    if (!success) {
+        gl.deleteShader(shader)
+        throw Error(`Shader compilation failed. ${gl.getShaderInfoLog(shader)}`)
+    }
+    return shader;
+}
---
+++ src/Beastiepedia/Preview/fragment.glsl
@@ -0,0 +1,67 @@
+precision mediump float;
+ 
+const int max_colors = 6;
+const vec3 color_tolorance = vec3(0.0833,0.99,0.99);
+
+// our texture
+uniform sampler2D u_image;
+uniform vec3 colorOut[max_colors];
+
+// the texCoords passed in from the vertex shader.
+varying vec2 v_texCoord;
+ 
+vec3 rgb_to_hsv( vec3 c ) {
+    vec4 K = vec4( 0., -1./3., 2./3., -1. );
+    vec4 p = (c.g < c.b)? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
+    vec4 q = (c.r < p.x)? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
+    float chroma = q.x - min(q.w, q.y);
+    if ( chroma == 0. ) //max = min (no saturation)
+    {
+        return vec3( 0., 0., q.x );
+    }
+    else if ( q.x == 0. ) //max = 0 (black)
+    {
+        return vec3( 0. );
+    }
+    else //Normal output, without div-by-zero errors! \o/
+    {
+        return vec3( abs( q.z + (q.w - q.y)/(6.*chroma) ), chroma/q.x, q.x );
+    }
+}
+
+vec3 hsv_to_rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main() {
+    vec3 colorIn[max_colors];
+    colorIn[0] = vec3(0.0, 1.0, 1.0);
+    colorIn[1] = vec3(0.33, 1.0, 1.0);
+    colorIn[2] = vec3(0.66, 1.0, 1.0);
+    colorIn[3] = vec3(0.166, 1.0, 1.0);
+    colorIn[4] = vec3(0.5, 1.0, 1.0);
+    colorIn[5] = vec3(0.833, 1.0, 1.0);
+    vec4 initialColor = texture2D(u_image, v_texCoord);
+    vec3 color = initialColor.rgb;
+    vec3 colorHSV = rgb_to_hsv(color);
+    for (int i=0; i < max_colors; i+=1) {
+        vec3 colorDelta = colorHSV - colorIn[i];
+        if (abs(colorDelta.r) > 0.5) colorDelta.r -= sign(colorDelta.r);
+        if (
+            abs(colorDelta.r) <= color_tolorance.r && 
+            abs(colorDelta.g) <= color_tolorance.g &&
+            abs(colorDelta.b) <= color_tolorance.b
+        ) {
+            vec3 colorOutHSV = rgb_to_hsv(colorOut[i]);
+            color = hsv_to_rgb(vec3(
+                mod(colorOutHSV.r + colorDelta.r, 1.0),
+                clamp(colorOutHSV.g + colorDelta.g, 0.0, 1.0),
+                clamp(colorOutHSV.b + colorDelta.b, 0.0, 1.0)
+            ));
+        }
+    }
+   // Look up a color from the texture.
+   gl_FragColor =  vec4(color, initialColor.a);
+}

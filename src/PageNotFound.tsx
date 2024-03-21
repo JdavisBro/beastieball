27f77dfebce4ad9e818af729fa66b12c5fa0267c
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2024-03-21 21:45:58 +0000

    update page not found OpenGraph

--- src/PageNotFound.tsx
+++ src/PageNotFound.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import OpenGraph from "./shared/OpenGraph";
 
 export default function PageNotFound(): React.ReactElement {
   const [col, setCol] = useState(Math.floor(Math.random() * 360));
@@ -15,26 +16,34 @@ export default function PageNotFound(): React.ReactElement {
     "--notfoundface-spr": `url(/gameassets/sprEmoji/${spr}.png)`,
   } as React.CSSProperties;
   return (
-    <div className="notfoundcontainer">
-      <h1>Page Not Found</h1>
-      <div
-        className="notfoundfacecontainer"
-        tabIndex={0}
-        role="button"
-        style={style}
-        onClick={handleRandomize}
-        onKeyDown={(event) => {
-          if (event.key == "Enter") {
-            handleRandomize();
-          }
-        }}
-      >
-        <div className="notfoundfacebg"></div>
-        <div className="notfoundface"></div>
+    <>
+      <OpenGraph
+        title={"Page Not Found - Beastieball Info"}
+        image="ball.png"
+        url=""
+        description="A website with data on Beastieball!"
+      />
+      <div className="notfoundcontainer">
+        <h1>Page Not Found</h1>
+        <div
+          className="notfoundfacecontainer"
+          tabIndex={0}
+          role="button"
+          style={style}
+          onClick={handleRandomize}
+          onKeyDown={(event) => {
+            if (event.key == "Enter") {
+              handleRandomize();
+            }
+          }}
+        >
+          <div className="notfoundfacebg"></div>
+          <div className="notfoundface"></div>
+        </div>
+        <h1>
+          <Link to="/">Go to Home</Link>
+        </h1>
       </div>
-      <h1>
-        <Link to="/">Go to Home</Link>
-      </h1>
-    </div>
+    </>
   );
 }

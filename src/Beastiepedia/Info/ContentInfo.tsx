78822af099bc48c7f5d55fc2f22b5704b234661f
Your Name <44114870+JdavisBro@users.noreply.github.com>
2024-06-15 01:03:02 +0100

    add metamorph to beastiepedia info

--- src/Beastiepedia/Info/ContentInfo.tsx
+++ src/Beastiepedia/Info/ContentInfo.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import StatDistribution from "./StatDistribution";
 import TextTag from "../../shared/TextTag";
 import styles from "../Content.module.css";
@@ -6,6 +8,7 @@ import MoveList from "./MoveList";
 import untyped_research_data from "../../data/research_data.json";
 import designers from "../../data/designers.json";
 import abilities from "../../data/abilities";
+import BEASTIE_DATA from "../../data/Beastiedata";
 
 const research_data: { [key: string]: number } = untyped_research_data;
 
@@ -25,6 +28,55 @@ function InfoBox(props: {
       </div>
     </div>
   );
+}
+
+type EvolutionType = {
+  condition: number[];
+  specie: string;
+  value: number[];
+};
+
+function findBeastiePreevolution(
+  beastieId: string,
+  targetBeastieId: string,
+): {
+  beastie: BeastieType;
+  evolution: EvolutionType;
+} | null {
+  const beastie = BEASTIE_DATA.get(beastieId);
+  if (!beastie || !beastie.evolution) {
+    return null;
+  }
+  const evolution = beastie.evolution.find(
+    (value) => value.specie == targetBeastieId,
+  );
+  if (evolution) {
+    return { beastie, evolution };
+  } else {
+    const newEvolution = beastie.evolution
+      .map((value) => {
+        return findBeastiePreevolution(value.specie, targetBeastieId);
+      })
+      .find((value) => value);
+    if (newEvolution) {
+      return newEvolution;
+    }
+    return null;
+  }
+}
+
+const EVOLUTION_CONDITIONS: { [key: number]: string } = {
+  // "Evolves from/to BEASTIE (below strings with {N} replaced with value index)"
+  0: "at level {0}",
+};
+
+function getEvoConditionString(evo: EvolutionType) {
+  return EVOLUTION_CONDITIONS[evo.condition[0]]
+    ? EVOLUTION_CONDITIONS[evo.condition[0]].replace(
+        /{(\d+)}/g,
+        (match, index) => String(evo.value[Number(index)]) ?? match,
+      )
+    : "idk when though";
 }
 
 export default function ContentInfo(props: Props): React.ReactNode {
@@ -80,12 +132,71 @@ export default function ContentInfo(props: Props): React.ReactNode {
     );
   }
 
+  const preEvo = findBeastiePreevolution(beastiedata.family, beastiedata.id);
+  const evolutions = beastiedata.evolution;
+  const evolutionBeasties = evolutions?.map((value) =>
+    BEASTIE_DATA.get(value.specie),
+  );
+  // if (
+  //   preEvolutionBeastie &&
+  //   preEvolutionBeastie.evolution &&
+  //   preEvolutionBeastie.id != beastiedata.id
+  // ) {
+  //   while (preEvolutionData == undefined) {
+  //     preEvolutionData = preEvolutionBeastie.evolution.find(
+  //       (value) => value.specie == beastiedata.id,
+  //     );
+  //     preEvolutionBeastie.evolution.forEach((value) => )
+  //     // preEvolutionBeastie = BEASTIE_DATA.get(preEvolutionData.specie);
+  //   }
+  // } else {
+  //   preEvolutionBeastie = null;
+  // }
+
+  console.log(preEvo);
   return (
     <div className={styles.info}>
       <div className={styles.inner}>
         <div className={styles.wrapinfoboxes}>
           <InfoBox header="Number" value={`#${beastiedata.number}`} />
           <InfoBox header="Name" value={beastiedata.name} />
+          <InfoBox
+            header="Metamorphosis"
+            value={
+              <>
+                {preEvo ? (
+                  <div>
+                    Metamorphs from{" "}
+                    <Link to={`/beastiepedia/${preEvo.beastie.name}`}>
+                      {preEvo.beastie.name}
+                    </Link>{" "}
+                    {getEvoConditionString(preEvo.evolution)}
+                  </div>
+                ) : (
+                  <div>Does not Metamorph from any Beastie</div>
+                )}
+                {evolutions ? (
+                  evolutionBeasties?.map((beastie, index) => {
+                    if (!beastie) {
+                      return null;
+                    }
+                    const evolution = evolutions[index];
+                    return (
+                      <div>
+                        Metamorphs into{" "}
+                        <Link to={`/beastiepedia/${beastie.name}`}>
+                          {beastie.name}
+                        </Link>{" "}
+                        {getEvoConditionString(evolution)}
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div>Does not Metamorph into any Beastie</div>
+                )}
+              </>
+            }
+          />
         </div>
 
         <InfoBox header="Description" value={beastiedata.desc} />

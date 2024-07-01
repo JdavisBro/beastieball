6b7291e07125176b9787a10211e3cb38b32b726c
Your Name <44114870+JdavisBro@users.noreply.github.com>
2024-07-01 11:50:56 +0100

    sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import * as fs from "fs";
+
+import beastie_data from "./src/data/beastie_data.json";
+
+const BEASTIE_DATA: Record<string, { name: string }> = beastie_data;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const env = loadEnv("mock", process.cwd(), "");
+  const url = env.VITE_URL != "" ? env.VITE_URL : "https://beastieball.info";
+  return {
+    plugins: [
+      react(),
+      {
+        name: "Sitemap",
+        closeBundle() {
+          const beasties: string[] = Object.keys(BEASTIE_DATA).map(
+            (id) => `${url}/beastiepedia/${BEASTIE_DATA[id].name}`,
+          );
+          const sitemap = `${url}/
+${url}/beastiepedia/
+${beasties.join("\n")}
+${url}/map/
+${url}/modding/
+${url}/modding/save/`;
+          fs.writeFile("dist/sitemap.txt", sitemap, () => {});
+          fs.writeFile(
+            "dist/robots.txt",
+            `Sitemap: ${url}/sitemap.txt`,
+            () => {},
+          );
+        },
+      },
+    ],
+  };
+});

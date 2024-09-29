dd01a093f247734543cd58087c25c3a7108e0e0f
Your Name <44114870+JdavisBro@users.noreply.github.com>
2024-09-30 00:55:48 +0100

    use memo for beastiedata to reduce rerendering

--- src/Beastiepedia/Beastiepedia.tsx
+++ src/Beastiepedia/Beastiepedia.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Header from "../shared/Header";
@@ -23,21 +23,20 @@ export default function Beastiepedia(): React.ReactNode {
   const orientation = useScreenOrientation();
   const { beastie }: { beastie?: string } = useParams();
 
-  let beastieid: string | null = null;
-
-  if (beastie !== null) {
-    BEASTIE_DATA.forEach((value, key) => {
-      if (value.name == beastie) {
-        beastieid = key;
+  const [beastieid, beastiedata] = useMemo(() => {
+    if (beastie !== null) {
+      for (const [key, value] of BEASTIE_DATA) {
+        if (value.name == beastie) {
+          return [key, value];
+        }
       }
-    });
-  }
+    }
+    return [undefined, undefined];
+  }, [beastie]);
 
   const [sidebarvisible, setSidebarvisible] = useState(
     !(beastieid !== null && orientation),
   ); // Selected beastie & portrait automatically hides sidebar
-
-  const beastiedata = beastieid != null ? BEASTIE_DATA.get(beastieid) : null;
 
   window.BEASTIE_DATA = BEASTIE_DATA;
   window.beastie = beastiedata;
@@ -45,23 +44,19 @@ export default function Beastiepedia(): React.ReactNode {
   return (
     <div className={styles.container}>
       <OpenGraph
-        title={
-          beastieid === null ? "Beastiepedia" : `${beastie} - Beastiepedia`
-        }
+        title={beastiedata ? `${beastie} - Beastiepedia` : "Beastiepedia"}
         image={
           beastiedata
             ? `icons/${beastiedata.name}.png`
             : "gameassets/sprMainmenu/0.png" // beastiepedia icon
         }
-        url={beastieid === null ? `beastiepedia/` : `beastiepedia/${beastie}`}
+        url={beastiedata ? `beastiepedia/${beastiedata.name}` : `beastiepedia/`}
         description={
-          beastiedata != null
-            ? beastiedata.desc
-            : "View information on Beasties!"
+          beastiedata ? beastiedata.desc : "View information on Beasties!"
         }
       />
       <Header
-        title={(beastie !== undefined ? `${beastie} - ` : "") + "Beastiepedia"}
+        title={(beastie ? `${beastie} - ` : "") + "Beastiepedia"}
         menuButton={true}
         menuButtonState={sidebarvisible}
         onMenuButtonPressed={() => setSidebarvisible((visible) => !visible)}

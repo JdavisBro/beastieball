136414d357a64790dab51214e2e02eb621b03a2a
Your Name <44114870+JdavisBro@users.noreply.github.com>
2025-01-10 11:27:56 +0000

    take into account shape layer z pos for extramarkers

--- scripts/ExtraMapMarkers.py
+++ scripts/ExtraMapMarkers.py
@@ -2,6 +2,8 @@ import sys
 import json
 from pathlib import Path
 
+import shapely
+
 gamedir = Path(sys.argv[1])
 
 OTHER_AREAS = [
@@ -21,6 +23,22 @@ with list((gamedir / "world_data").glob("*world.json"))[0].open() as f:
 
 level_stumps = {stump["name"]: stump for stump in world_data["level_stumps_array"]}
 
+def find_z_pos(x, y, shape_groups):
+  highest_z = 0
+  for group in shape_groups:
+    grp_x, grp_y = group.get("x", 0), group.get("y", 0)
+    for shape in group.get("shapes_array", []):
+      shp_x, shp_y = shape.get("x", 0), shape.get("y", 0)
+      pts = shape.get("points_array", [])
+      poly = shapely.Polygon([
+          [grp_x + shp_x + pts[i], grp_y + shp_y + pts[i+1]]
+          for i in range(0, len(pts), 3)
+      ])
+      if shapely.contains_xy(poly, x, y):
+        highest_z = max(highest_z, group.get("z", 0) + shape.get("z", 0))
+  return highest_z
+
+
 for level in (gamedir / "world_data").glob("**/*.json"):
   if "world" in level.name or "zerozero" in level.name:
     continue
@@ -33,8 +51,12 @@ for level in (gamedir / "world_data").glob("**/*.json"):
   layer = stump["world_layer"] if "world_layer" in stump else 0 
   level_offset = ([area["offset"] for area in OTHER_AREAS if level_name.startswith(area["prefix"])] or [[0, 0]])[0] if layer != 0 else [0,0]
   for obj in level_data["objects_array"]:
+    z = find_z_pos(
+      obj.get("x", 0),
+      obj.get("y", 0),
+      level_data.get("shape_groups_array", []))
     x = stump["world_x1"] + obj["x"] + level_offset[0]
-    y = stump["world_y1"] + obj["y"] - (obj["z"] if "z" in obj else 0) + level_offset[1]
+    y = stump["world_y1"] + obj["y"] - (obj.get("z", 0)) - z + level_offset[1]
     if obj["object"] == "objGift":
       gift_id = f"gift_{level_data['name']}_{obj['gift_id_index']}" if obj["gift_id"] == "none" else obj['gift_id']
       if gift_id not in gift_data:

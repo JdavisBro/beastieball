c5e46a1bbbcd1bff705f4814a6d80438a4162a58
Your Name <44114870+JdavisBro@users.noreply.github.com>
2025-06-03 00:12:04 +0100

    add recently updated

--- src/Playdex/RecentlyUpdated.ts
+++ src/Playdex/RecentlyUpdated.ts
@@ -1,3 +1,15 @@
-const MOVE_RECENTLY_UPDATED: string[] = [];
+const MOVE_RECENTLY_UPDATED: string[] = [
+  "contest",
+  "cook",
+  "dare",
+  "facade",
+  "flatten",
+  "jazzattack",
+  "lifepass",
+  "rest",
+  "rocket",
+  "scream",
+  "snore",
+];
 
 export default MOVE_RECENTLY_UPDATED;

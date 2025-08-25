a93f87fd80b21f6e397651b86fcd37bfe7f50935
Your Name <44114870+JdavisBro@users.noreply.github.com>
2025-08-25 19:03:17 +0100

    update recently updated

--- src/Playdex/RecentlyUpdated.ts
+++ src/Playdex/RecentlyUpdated.ts
@@ -1,3 +1,23 @@
-const MOVE_RECENTLY_UPDATED: string[] = [];
+const MOVE_RECENTLY_UPDATED: string[] = [
+  "basecover",
+  "callout",
+  "defense",
+  "doubleblock",
+  "hex",
+  "hyperbeam",
+  "libero",
+  "mark",
+  "markattack",
+  "menace",
+  "mimic",
+  "myheartpass",
+  "preblock",
+  "tailwind",
+  "taunt",
+  "thrash",
+  "toughattack",
+  "wall",
+  "wornattack",
+];
 
 export default MOVE_RECENTLY_UPDATED;

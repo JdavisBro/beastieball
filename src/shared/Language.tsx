d04336abad1b336a56a2198003fcde614d753d1a
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2026-01-18 22:10:58 +0000

    add unsupported language message

--- src/shared/Language.tsx
+++ src/shared/Language.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useLocalization, {
   LANGUAGE_NAMES,
   SUPPORTED_LANGUAGES,
@@ -8,26 +9,40 @@ export function Language() {
   const { L, currentLanguage, setLanguage } = useLocalization();
 
   return (
-    <label>
-      {L("common.languageLabel")}
-      <select
-        value={currentLanguage}
-        onChange={(event) =>
-          setLanguage(
-            SUPPORTED_LANGUAGES.includes(
-              event.target.value as SupportedLanguage,
+    <>
+      <label>
+        {L("common.languageLabel")}
+        <select
+          value={currentLanguage}
+          onChange={(event) =>
+            setLanguage(
+              SUPPORTED_LANGUAGES.includes(
+                event.target.value as SupportedLanguage,
+              )
+                ? (event.target.value as SupportedLanguage)
+                : "en",
             )
-              ? (event.target.value as SupportedLanguage)
-              : "en",
-          )
-        }
-      >
-        {SUPPORTED_LANGUAGES.map((lang) => (
-          <option key={lang} value={lang}>
-            {LANGUAGE_NAMES[lang]}
-          </option>
-        ))}
-      </select>
-    </label>
+          }
+        >
+          {SUPPORTED_LANGUAGES.map((lang) => (
+            <option key={lang} value={lang}>
+              {LANGUAGE_NAMES[lang]}
+            </option>
+          ))}
+        </select>
+      </label>
+      {currentLanguage != "en" ? (
+        <div
+          style={{
+            maxWidth: "220px",
+            backgroundColor: "rgba(0,0,0,0.1)",
+          }}
+        >
+          This language is not fully supported yet. If you know English and this
+          language please reach out to help translate the site at{" "}
+          <Link to={import.meta.env.VITE_ISSUES_URL}>GitHub issues</Link>.
+        </div>
+      ) : null}
+    </>
   );
 }

f16acadae528f846fac8f89be84e2d2f48b8a552
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2026-05-13 19:31:11 +0100

    add og for Daily

--- src/Daily/Daily.tsx
+++ src/Daily/Daily.tsx
@@ -1,8 +1,20 @@
+import useLocalization from "../localization/useLocalization";
 import { NavigationMenu, NavigationMenuOption } from "../shared/NavigationMenu";
+import OpenGraph from "../shared/OpenGraph";
 
 export default function Daily() {
+  const { L } = useLocalization();
   return (
     <NavigationMenu title="Dailies">
+      <OpenGraph
+        title={L("common.title", {
+          page: "Daily",
+          branding: import.meta.env.VITE_BRANDING,
+        })}
+        image={"gameassets/sprMainmenu/25.png"}
+        url={"daily/"}
+        description={"Daily games for Beastieball"}
+      />
       <NavigationMenuOption
         text="Beastdle"
         image="/gameassets/sprMainmenu/25.png"
